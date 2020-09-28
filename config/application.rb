28165c0d791603b964856100fb38242e2dc31d51
bzumpano <zumpano.lp@gmail.com>
2020-09-28 00:42:59 -0300

    setup project defaults

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Nexaas
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    config.encoding = 'utf-8'
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
   end
 end
